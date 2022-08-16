a500d7e58a719ca3718ff18d515d47a7a781398f
jsjohnn <63035520+jsjohnn@users.noreply.github.com>
2022-08-16 18:00:49 +0900

    build6

--- client/src/page/Result.jsx
+++ client/src/page/Result.jsx
@@ -94,7 +94,7 @@ const Result = () => {
     'super-name',
     () => {
       return axios.get(
-        `https://localhost:5000/api/food/result?mood=${answerData.mood}&age=${answerData.age}&money=${answerData.money}&ingredient=${answerData.ingredient}`,
+        `http://localhost:5000/api/food/result?mood=${answerData.mood}&age=${answerData.age}&money=${answerData.money}&ingredient=${answerData.ingredient}`,
       );
     },
     { staleTime: Infinity },
